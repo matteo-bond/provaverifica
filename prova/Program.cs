b9ffb7d60c758e534c6852e5b7e669a348f18a7f
Matteo Bondi <MATTEO.BONDI@local.ittcesena.it>
2025-11-14 08:36:18 +0100

    ver2

---
+++ prova/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace prova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, say your name");
+            string name = Console.ReadLine();
+        }
+    }
+}
